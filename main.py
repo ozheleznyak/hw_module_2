cfd93ddfeb94c70c053d4d19e6a4553ff22404f7
olga_zhe <olgazheleznyak@gmail.com>
2025-08-24 16:51:38 +0300

    function get_date released

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from src import masks
+from src import widget
 
-user_card_number = input("Enter your card number (16 digits without spaces): ")
-
-print(masks.get_mask_card_number(user_card_number))
-
-user_account_number = input("Enter your account number (only digits): ")
-
-print(masks.get_mask_account(user_account_number))
+if __name__ == "__main__":
+    # user_card_number = input("Enter your card number (16 digits without spaces): ")
+    # print(masks.get_mask_card_number(user_card_number))
+    # user_account_number = input("Enter your account number (only digits): ")
+    # print(masks.get_mask_account(user_account_number))
+    transaction_date_format = input("Enter your transaction timestamp: ")
+    print(widget.get_date(transaction_date_format))
---
+++ src/widget.py
@@ -0,0 +1,19 @@
+from src import masks
+def mask_account_card():
+    """"""
+
+
+def get_date(transaction_date_format):
+    """функция для преобразования таймстэмпа в формат DD.MM.YYYY без указания времени
+    реализовано с учетом того, что у формата даты есть определнный шаблон"""
+    only_date_by_user = ""
+    i = 0
+
+    while transaction_date_format[i] != "T":
+        only_date_by_user += transaction_date_format[i]
+        i += 1
+
+    only_date_by_user_list = only_date_by_user.split("-")
+    transformed_date_list = ".".join(only_date_by_user_list[-1::-1])
+
+    return transformed_date_list
