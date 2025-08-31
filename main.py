1335cf27dd1d8b5f4cacb8c3964ff0712e0c2a1c
olga_zhe <olgazheleznyak@gmail.com>
2025-08-31 17:13:33 +0300

    minor fixes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # from src import masks
-from src import widget
+# from src import widget
+from src import processing
 
 if __name__ == "__main__":
     # user_card_number = input("Enter your card number (16 digits without spaces): ")
@@ -8,5 +9,13 @@ if __name__ == "__main__":
     # print(masks.get_mask_account(user_account_number))
     # transaction_date_format = input("Enter your transaction timestamp: ")
     # print(widget.get_date(transaction_date_format))
-    user_input = input("Enter your account or card number: ")
-    print(widget.mask_account_card(user_input))
+    # user_input = input("Enter your account or card number: ")
+    # print(widget.mask_account_card(user_input))
+
+    #
+    user_input_transaction_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+    user_input_state = (input("Enter filter state or press Enter for default filter by EXECUTED: "))
+    if user_input_state:
+        print(processing.filter_by_state(user_input_transaction_list, user_input_state))
+    else:
+        print(processing.filter_by_state(user_input_transaction_list))
--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(dict_list: list, state='EXECUTED') -> list:
+def filter_by_state(dict_list: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция возвращает новый список словарей, содержащий только те словари,
     у которых ключ state соответствует указанному значению"""
     filtered_by_state_dict_list = []
@@ -10,7 +10,3 @@ def filter_by_state(dict_list: list, state='EXECUTED') -> list:
     return filtered_by_state_dict_list
 
 
-dict_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-
-print(filter_by_state(dict_list, "CANCELED"))
-git
