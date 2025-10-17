1ad465aed761a10799d0e067a528d5a7d6386f87
olga_zhe <olgazheleznyak@gmail.com>
2025-10-17 16:51:24 +0300

    external api func added

---
+++ src/external_api.py
@@ -0,0 +1,17 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+def external_api_exchange(amount: str, from_currency: str):
+    load_dotenv()
+    api_key = os.getenv("API_KEY")
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={from_currency}&amount={amount}&apikey={api_key}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return round(response.json()['result'], 2)
+    else:
+        raise ValueError(f"Failed to get currency rate")
+
+
+# print(external_api_exchange("1000", "USD"))
